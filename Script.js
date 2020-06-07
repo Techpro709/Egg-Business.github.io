dd438f8ea23d4d4b0c7714d4b171323b5ae55e20
Techpro709 <61668117+Techpro709@users.noreply.github.com>
2020-06-06 20:26:16 -0700

    Uploaded Files

---
+++ Script.js
@@ -0,0 +1,41 @@
+// JavaScript source code
+var bank = parseInt(localStorage.getItem("bankValue"));
+var inputValue = 0;
+
+function count() {
+    inputValue = parseInt(document.getElementById("moneyInput").value);
+    //clear console
+    console.clear()
+    //button pushed 
+    console.log("Recive has been pushed");
+    //what is inputvalue
+
+
+    //telling user what bank,inputvalue and new bank is
+    console.log(bank);
+    console.log("Input value is equal to " + inputValue);
+    bank += inputValue;
+    console.log("Updated");
+    console.log("bank = " + bank);
+    // updating localStorage
+
+    // Check browser support
+    if (typeof (Storage) !== "undefined") {
+        // Store
+        localStorage.setItem("bankValue", bank);
+        // Retrieve
+        document.getElementById("bank_value").innerHTML = parseInt(localStorage.getItem("bankValue"));
+    } else {
+        document.getElementById("bank_value").innerHTML = "Sorry, your browser does not support Web Storage...";
+    }
+}
+function display() {
+    document.getElementById("bank_value").innerHTML = localStorage.getItem("bankValue");
+
+}
+function reset() {
+    console.log("Resetting");
+    bank = 0;
+    localStorage.setItem("bankValue", bank);
+    display()
+}
