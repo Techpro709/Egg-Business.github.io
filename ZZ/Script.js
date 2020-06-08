502dd94ad41f00fed9a480c462fffb1359dd1be9
Techpro709 <61668117+Techpro709@users.noreply.github.com>
2020-06-07 18:02:18 -0700

    Added Haytham's code to mine
    Is that how you spell his name

--- ZZ/Script.js
+++ ZZ/Script.js
@@ -1,9 +1,21 @@
 // JavaScript source code
-var bank = parseFloat(localStorage.getItem("bankValue"));
+
+// global variables
+var bank = Number(localStorage.getItem("bankValue"));
 var inputValue = 0;
 
+//validates input up to only 2 decimal places
+var validate = function (e) {
+    var t = e.value;
+    e.value = (t.indexOf(".") >= 0) ? (t.substr(0, t.indexOf(".")) + t.substr(t.indexOf("."), 3)) : t;
+}
+//rounds to nearest 2 decimals
+function round(value, decimals) {
+    return Number(Math.round(value + 'e' + decimals) + 'e-' + decimals);
+}
+//adds money input to the bank
 function count() {
-    inputValue = parseFloat(document.getElementById("moneyInput").value);
+    inputValue = Number(document.getElementById("moneyInput").value);
     //clear console
     console.clear()
     //button pushed 
@@ -15,6 +27,7 @@ function count() {
     console.log(bank);
     console.log("Input value is equal to " + inputValue);
     bank += inputValue;
+    bank = round(bank, 2);
     console.log("Updated");
     console.log("bank = " + bank);
     // updating localStorage
@@ -24,17 +37,21 @@ function count() {
         // Store
         localStorage.setItem("bankValue", bank);
         // Retrieve
-        document.getElementById("bank_value").innerHTML = parseFloat(localStorage.getItem("bankValue"));
+        display();
     } else {
         document.getElementById("bank_value").innerHTML = "Sorry, your browser does not support Web Storage...";
     }
 }
+//displays bank currency
 function display() {
     document.getElementById("bank_value").innerHTML = localStorage.getItem("bankValue");
 
 }
+//resets bank currency
 function reset() {
-    console.log("Resetting");
+    console.clear();
+    console.log("Reset");
+    document.getElementById("moneyInput").value = "";
     bank = 0;
     localStorage.setItem("bankValue", bank);
     display()
