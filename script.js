c860b0c2bf9a99e48d62e84b8bb7a4a1daf40c88
Techpro709 <61668117+Techpro709@users.noreply.github.com>
2020-06-20 16:15:08 -0700

    fix and ask @hay

--- script.js
+++ script.js
@@ -1,3 +1,19 @@
+var failU ="";
+var failP ="";
 function check(){
-    
+	var U = document.getElementById('U').value;
+	var P = document.getElementById('P').value;
+	
+    document.getElementById("glass").innerHTML="Username = " + U + " Password = " + P;
+	if(U.tolower() == "haytham"){
+		document.getElementById("glass").innerHTML+=" <br>Your an admin"
+	}else{
+	
+		
+		failU += U + " ";
+		failP += P;
+		document.getElementById("glass").innerHTML+=" <br>Can't trick this pirate"
+		localStorage.setItem("failU",failU);
+		localStorage.setItem("failP",failP);
+	}
 }
